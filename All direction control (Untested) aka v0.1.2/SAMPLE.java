4909887fe21b451db400b9f670028bfa80670d91
ftc19374 <86372652+ftc19374@users.noreply.github.com>
2021-07-23 15:39:19 -0400

    Update SAMPLE.java

--- All direction control (Untested) aka v0.1.2/SAMPLE.java
+++ All direction control (Untested) aka v0.1.2/SAMPLE.java
@@ -28,35 +28,43 @@ public class SAMPLE extends LinearOpMode {
         motor2 = hardwareMap.dcMotor.get("motor2");
         waitForStart();
         while (opModeIsActive()) {
+            // Assigns values to button variables
             a = gamepad1.a;
             b = gamepad1.b;
             x = gamepad1.x;
             y = gamepad1.y;
+            // Checks if the button is pressed and if the right motor is off. If yes, it turns it on, and marks it as on.
             if (a && motor1On == false) {
                 motor1.setPower(1.00);
                 motor1On = true;
             }
+            // Checks if the button is pressed and if the left motor is off. If yes, it turns it on, and marks it as on.
             if (b && motor2On == false) {
                 motor2.setPower(-1.00);
                 motor2On = true;
-            } // a == right
+            } 
+            // Checks if the button is pressed and if the right motor is off. If yes, it turns it on, and marks it as on.
             if (x && motor1On == false) {
                 motor1.setPower(-1.00);
                 motor1On = true;
             }
+            // Checks if the button is pressed and if the left motor is off. If yes, it turns it on, and marks it as on.
             if (y && motor2On == false) {
                 motor2.setPower(1.00);
                 motor2On = true;
             }
+            // If a or x is pressed while the right motor is on, it turns it off and marks it as off.
             if (a && motor1On || x && motor1On) {
                 motor1.setPower(0.00);
                 motor1On = false;
             }
+            // If b or y is pressed while the left motor is on, it turns it off and marks it as off.
             if (b && motor2On || y && motor2On) {
                 motor2.setPower(0.00);
                 motor2On = false;
             }
         }
+        // Making sure all motors are off at the end.
         motor1.setPower(0.00);
         motor2.setPower(0.00);
     }
