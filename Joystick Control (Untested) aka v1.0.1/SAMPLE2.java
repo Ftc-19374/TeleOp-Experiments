3c82fb98666c99df1d4097735e18fe65ef0a731d
Jaws-all <77344665+Jaws-all@users.noreply.github.com>
2021-07-24 15:21:47 -0400

    Update SAMPLE2.java
    Same as sample.java

--- Joystick Control (Untested) aka v1.0.1/SAMPLE2.java
+++ Joystick Control (Untested) aka v1.0.1/SAMPLE2.java
@@ -10,7 +10,7 @@ import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.I2cAddr;
 
 @TeleOp(name="Driving in all directions")
-public class SAMPLE extends LinearOpMode {
+public class SAMPLE2 extends LinearOpMode {
     // Variable creations
     double x;
     double y;
@@ -25,8 +25,8 @@ public class SAMPLE extends LinearOpMode {
         while (opModeIsActive()) {
             x = gamepad1.left_stick_y;
             y = gamepad1.left_stick_x;
-            motor1.setPower(x - (0.5 * y));
-            motor2.setPower(-x - (0.5 * y));
+            motor1.setPower(Range.clip(-x + (0.5 * y), -1.0, 1.0);
+            motor2.setPower(Range.clip(x + (0.5 * y), -1.0, 1.0);
 
         }
         motor1.setPower(0.00);
